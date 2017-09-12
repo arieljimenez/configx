d5e8f6f78cc6b5541497f824d498146c9fde0542
Ariel Jimenez <ajimenez@intellisys.com.do>
2017-09-12 00:26:55 -0400

    Add instructions to Code dirs

--- elementary.loki.0.4.1.sh
+++ elementary.loki.0.4.1.sh
@@ -5,5 +5,12 @@
 # TODO: check if ~/.ssh exist to continue else BREAK
 sudo apt update && sudo apt upgrade -y && sudo apt auto-remove -y
 
+# Dirs
+mkdir -p ~/Code/js \
+         ~/Code/go/src/frismaury \
+         ~/Code/go/bin
+
+# TODO: PATH stuff
+
 # Install aps
 . ./apps/apps.sh
