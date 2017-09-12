0139d2b9c09eba9bbd97712999623127cde05b23
Ariel Jimenez <ajimenez@intellisys.com.do>
2017-09-11 23:34:53 -0400

    Add a specific sh to Elementary Distro

---
+++ elementary.loki.0.4.1.sh
@@ -0,0 +1,9 @@
+# 09-09-2017
+# Download .ssh dir from Gdrive first
+# before start, update the system
+
+# TODO: check if ~/.ssh exist to continue else BREAK
+sudo apt update && sudo apt upgrade -y && sudo apt auto-remove -y
+
+# Install aps
+. ./apps/apps.sh
