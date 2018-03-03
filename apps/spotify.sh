a2e019bb523f500688708278640299a497d86584
Ariel Jimenez <ajimenez@intellisys.com.do>
2018-03-03 11:40:56 -0400

    Add spotify support for local files

--- apps/spotify.sh
+++ apps/spotify.sh
@@ -10,3 +10,10 @@ sudo apt-get update
 
 # 4. Install Spotify
 sudo apt-get install spotify-client -y
+
+# support local files
+sudo apt install ubuntu-restricted-extras && \
+  sudo wget -N https://github.com/ramedeiros/spotify_libraries/raw/master/libavcodec.so.54.71.100 -O /usr/lib/x86_64-linux-gnu/libavcodec.so.54 && \
+  sudo wget -N https://github.com/ramedeiros/spotify_libraries/raw/master/libavformat.so.54.36.100 -O /usr/lib/x86_64-linux-gnu/libavformat.so.54 && \
+  sudo wget -N https://github.com/ramedeiros/spotify_libraries/raw/master/libavutil.so.52.6.100 -O /usr/lib/x86_64-linux-gnu/libavutil.so.52 && \
+  sudo ldconfig
