9822172b1332be8da33da40c98a7f0ddecc33e0c
Ariel Jimenez <ajimenez@intellisys.com.do>
2018-03-13 00:37:33 -0400

    Update LoL installation

--- apps/LeagueOfLegends.sh
+++ apps/LeagueOfLegends.sh
@@ -7,50 +7,42 @@
 ## nVidia 375 drivers
 ################################################
 
-sudo dpkg --add-architecture i386 && \
-    wget -nc https://dl.winehq.org/wine-builds/Release.key && \
-    sudo apt-key add Release.key
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/Release.key
+sudo apt-key add Release.key
+sudo apt install software-properties-common
+sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+sudo apt-get update && sudo apt upgrade -y
+sudo apt-get install --install-recommends winehq-staging
 
-sudo apt install software-properties-common -y && \
-    sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/ && \
-    sudo apt-get update && sudo apt upgrade -y && \
-    sudo apt-get install --install-recommends \
-        winehq-staging \
-        cabextract \
-        -y
+mkdir ~/.wineDir
+cd ~/.wineDir
+wget https://riotgamespatcher-a.akamaihd.net/ShellInstaller/LA1/LeagueofLegends_LA1_Installer_2016_05_26.exe
+mv LeagueofLegends_LA1_Installer_2016_05_26.exe Lol.exe
+WINEPREFIX=~/.wineDir WINEARCH=win32 wine winecfg
+WINEPREFIX=~/.wineDir ./winetricks d3dx9
+WINEPREFIX=~/.wineDir wine winecfg
 
-mkdir ~/LoL32 && \
-    cd ~/LoL32 && \
-    wget https://riotgamespatcher-a.akamaihd.net/releases/live/installer/deploy/League%20of%20Legends%20installer%20LA1.exe && \
-    mv League\ of\ Legends\ installer\ LA1.exe Lol.exe && \
-    wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks && \
-    chmod +x winetricks
-
-WINEPREFIX=~/LoL32 WINEARCH=win32 wine winecfg
-WINEPREFIX=~/LoL32 ./winetricks d3dx9
-WINEPREFIX=~/LoL32 wine winecfg
-
-#With winecfg open, it’s now time to change a few settings. First, change the Windows version to XP. Then, Click on the graphics tab and check off "Automatically capture mouse in full screen windows." Next, you need to set League to run on a virtual desktop. Check off "emulate a virtual desktop" and then set the resolution to your monitors native resolution. For example, the native resolution of a 1080p monitor is 1920x1080 (1440x900 / 1200x800). It is important to do this because certain glitches may occur otherwise. It is absolutely critical you do this if you have multiple monitors, as League may open on the wrong monitor and will not lock the mouse otherwise. Moving on, click the staging tab and enable CSMT. This will give a performance boost in most cases, but may cause the game to crash on some systems. If you cannot load into a game, disable this setting. Finally, click on the libraries tab and use the “New override for libraries” text box to add the override msvcp140. With these settings changed, you may now click the apply button in winecfg and then click ok. Next time you open wine on the prefix LoL32 the screen will be completely blue, this means the virtual desktop was enabled properly.
+#With winecfg open, it’s now time to change a few settings. First, change the Windows version to XP. Then, Click on the graphics tab and check off "Automatically capture mouse in full screen windows." Next, you need to set League to run on a virtual desktop. Check off "emulate a virtual desktop" and then set the resolution to your monitors native resolution. For example, the native resolution of a 1080p monitor is 1920x1080. It is important to do this because certain glitches may occur otherwise. It is absolutely critical you do this if you have multiple monitors, as League may open on the wrong monitor and will not lock the mouse otherwise. Moving on, click the staging tab and enable CSMT. This will give a performance boost in most cases, but may cause the game to crash on some systems. If you cannot load into a game, disable this setting. Finally, click on the libraries tab and use the “New override for libraries” text box to add the override msvcp140. With these settings changed, you may now click the apply button in winecfg and then click ok. Next time you open wine on the prefix .wineDir the screen will be completely blue, this means the virtual desktop was enabled properly.
 #Once again, if you had any strange errors in this section of the guide, I recommend you ask them on /r/linuxquestions. Otherwise, continue to chapter 3. Any questions from future chapters should be asked in this thread first, as I may be able to help.
 
-cd ~ && \
-    mkdir LoL_Installer && \
-    WINEPREFIX=~/LoL32 wine ~/LoL32/Lol.exe /extract:Z:/home/$USER/LoL_Installer && \
-    WINEPREFIX=~/LoL32 wine ~/LoL_Installer/vcredist_x86.exe && \
-    cd ~/LoL_Installer
-
+cd ~/.wineDir
+mkdir temp
+WINEPREFIX=~/.wineDir wine ~/.wineDir/Lol.exe /extract:Z:/home/ariel/.wineDir/temp
+WINEPREFIX=~/.wineDir wine ~/temp/vcredist_x86.exe
+cd ~/temp
 # Execute the installer
-WINEPREFIX=~/LoL32 wine msiexec /i LoL.LA1.msi
+WINEPREFIX=~/.wineDir wine msiexec /i LoL.LA1.msi
 #After install, check  "Open the game after install"
 # if not, run de Launcher
-WINEPREFIX=~/LoL32 wine "C:/Riot Games/League of Legends/lol.launcher.exe"
+WINEPREFIX=~/.wineDir wine "C:/Riot Games/League of Legends/lol.launcher.exe"
 
 # or run the game
-WINEPREFIX=~/LoL32 wine "C:/Riot Games/League of Legends/LeagueClient.exe"
+WINEPREFIX=~/.wineDir wine "C:/Riot Games/League of Legends/LeagueClient.exe"
 
 # either run the launcher or client will consume your tty, but wineserver will use to "throw" debug logs and stuff here.
 # So, to kill the client
-WINEPREFIX=~/LoL32 wineserver -k
+WINEPREFIX=~/.wineDir wineserver -k
 
-#PD: after the game installs, we can delete ~/LoL_Installer dir.
+#PD: after the game installs, we can delete THE CONTENT of ~/temp dir.
 #PD2: Make an alias for run and kill lol
