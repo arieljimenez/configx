aef22b4fe505809cb489d4fae37764fcc9558b02
Ariel Jimenez <ariel.jimenez2025@gmail.com>
2018-10-22 16:27:48 -0400

    Update vscode plugins

--- apps/code/code.sh
+++ apps/code/code.sh
@@ -5,37 +5,37 @@
 code --install-extension andyyaldoo.vscode-json
 code --install-extension lamartire.git-indicators
 code --install-extension bierner.markdown-emoji
-code --install-extension WakaTime.vscode-wakatime
+# code --install-extension WakaTime.vscode-wakatime
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension Tyriar.sort-lines
 code --install-extension robertohuertasm.vscode-icons
 code --install-extension eamodio.gitlens
 code --install-extension PeterJausovec.vscode-docker
-code --install-extension michelemelluso.code-beautifier
-code --install-extension HookyQR.minify
+# code --install-extension michelemelluso.code-beautifier
+# code --install-extension HookyQR.minify
 code --install-extension techer.open-in-browser
 code --install-extension formulahendry.auto-close-tag
-code --install-extension MS-vsliveshare.vsliveshare
+# code --install-extension MS-vsliveshare.vsliveshare
 
 ## PYTHON
-code --install-extension ms-python.python
-code --install-extension wholroyd.jinja
+# code --install-extension ms-python.python
+# code --install-extension wholroyd.jinja
 
 ## GO
-code --install-extension lukehoban.go
+# code --install-extension lukehoban.go
 
 ## CSS
-code --install-extension mrmlnc.vscode-autoprefixer
-code --install-extension sysoev.language-stylus
-code --install-extension konstantinkai.vscode-css-to-stylus
+# code --install-extension mrmlnc.vscode-autoprefixer
+# code --install-extension sysoev.language-stylus
+# code --install-extension konstantinkai.vscode-css-to-stylus
 code --install-extension Zignd.html-css-class-completion
 # necinc.elmmet
 # ritwickdey.live-sass
 
 ## ELM
-code --install-extension sbrink.elm
-code --install-extension Rubymaniac.vscode-html-to-elm
-code --install-extension joeandaverde.vscode-elm-jump
+# code --install-extension sbrink.elm
+# code --install-extension Rubymaniac.vscode-html-to-elm
+# code --install-extension joeandaverde.vscode-elm-jump
 
 ## JSX
 code --install-extension vsmobile.vscode-react-native
